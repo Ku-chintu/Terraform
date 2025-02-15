cb9aeeadf9d124ebf6c69678b945aedfdd67459e
ku-chintu <kumar.chintu1987@gmail.com>
2025-02-15 22:57:53 +0530

    modified state file

--- DAY-4-BACKEND-STATE-FILE/backend.tf
+++ DAY-4-BACKEND-STATE-FILE/backend.tf
@@ -1,6 +1,6 @@
 terraform {
 backend "s3" {
-    bucket         = "chintu1985"  
+    bucket         = "chintu1989"  
     region         =  "ap-south-1"
     key            = "terraform.tfstate" 
     dynamodb_table = "terraform-state-lock-dynamo" 
