32c7f4f2a52b134c361bb0562682b31742d51f53
ku-chintu <kumar.chintu1987@gmail.com>
2025-02-20 15:05:03 +0530

    updated files

--- DAY-5-Custom-vpc-project/custom-network.tf
+++ DAY-5-Custom-vpc-project/custom-network.tf
@@ -19,7 +19,6 @@ resource "aws_subnet" "public-subnet" {
     vpc_id = aws_vpc.project.id
     availability_zone = "ap-south-1a"
     cidr_block = "10.0.0.0/24"
-    map_public_ip_on_launch = true
     tags = {
       Name = "public-subnet"
     }
