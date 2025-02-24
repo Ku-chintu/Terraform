785cfc0703498c789a3b552ddaf0ed4827142b63
ku-chintu <kumar.chintu1987@gmail.com>
2025-02-25 00:42:07 +0530

    dependent-block

---
+++ DAY-10-Dependent-block/main.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "dependent-block"  
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-0d682f26195e9ec0f"
+    instance_type = "t2.micro"
+    depends_on = [ aws_s3_bucket.example]
+    tags = {
+      Name = "ec2-server"
+    }
+}
