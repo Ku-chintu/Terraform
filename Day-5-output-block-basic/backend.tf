3eebbf0f367eef13edea2b11de435957a41ca259
ku-chintu <kumar.chintu1987@gmail.com>
2025-02-25 13:26:48 +0530

    pending files

--- Day-5-output-block-basic/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-backend "s3" {
-    bucket         = "terraform-day-5"  
-    region         =  "us-east-1"
-    key            = "terraform.tfstate"    
-    dynamodb_table = "terraform-state-lock-dynamo" 
-    encrypt        = true
-}
-}
